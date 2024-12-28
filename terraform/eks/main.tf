aac693f00a67af9d14e8e7caf165153b0985887a
MH <mh @gmail com>
2024-12-28 11:44:13 +1100

    Added helm provisioning for Stream and Edge

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -8,6 +8,38 @@ variable "demo_name_prefix" {
   description = "Give it a name we can recognise in AWS EC2 console"
 }
 
+variable "cribl_edge_leader_url" {
+  description = "The leader URL for the Cribl Edge"
+}
+
+variable "cribl_edge_token" {
+  description = "The token for the Cribl Edge"
+}
+
+variable "cribl_edge_version" {
+  description = "The version of the Cribl Edge"
+}
+
+variable "cribl_edge_fleet" {
+  description = "The fleet name for the Cribl Edge"
+}
+
+variable "cribl_stream_leader_url" {
+  description = "The leader URL for the Cribl Stream"
+}
+
+variable "cribl_stream_token" {
+  description = "The token for the Cribl Stream"
+}
+
+variable "cribl_stream_version" {
+  description = "The version of the Cribl Stream"
+}
+
+variable "cribl_stream_worker_group" {
+  description = "The worker group for the Cribl Stream"
+}
+
 provider "aws" {
   region = var.region
 }
@@ -103,31 +135,90 @@ module "eks" {
   }
 }
 
-output "cluster_id" {
-  description = "AWS EKS Cluster ID"
-  value       = module.eks.cluster_id
+resource "null_resource" "update_kubeconfig" {
+  provisioner "local-exec" {
+    command = "aws eks --region ${var.region} update-kubeconfig --name ${module.eks.cluster_name}"
+  }
+
+  depends_on = [module.eks]
 }
 
-output "cluster_name" {
-  description = "AWS EKS Cluster ID"
-  value       = module.eks.cluster_name
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      command     = "aws"
+    }
+  }
 }
 
-output "cluster_endpoint" {
-  description = "AWS EKS Cluster Endpoint"
-  value       = module.eks.cluster_endpoint
+resource "helm_release" "cribl_worker" {
+  
+  name       = "cribl-worker"
+  repository = "https://criblio.github.io/helm-charts/"
+  chart      = "logstream-workergroup"
+  version    = "${var.cribl_stream_version}"
+  namespace  = "cribl"
+  create_namespace = true
+
+  depends_on = [null_resource.update_kubeconfig]
+
+  set {
+    name  = "config.host"
+    value = "${var.cribl_stream_leader_url}"
+  }
+
+  set {
+    name  = "config.token"
+    value = "${var.cribl_stream_token}"
+  }
+
+  set {
+    name  = "config.group"
+    value = "${var.cribl_stream_worker_group}"
+  }
+
+  set {
+    name  = "config.tlsLeader.enable"
+    value = "true"
+  }
+
+  set {
+    name  = "env.CRIBL_K8S_TLS_REJECT_UNAUTHORIZED"
+    value = "0"
+  }
+
+  set {
+    name  = "env.CRIBL_MAX_WORKERS"
+    value = "4"
+  }
+
+  values = [ file("${path.module}/../../cribl/stream/values.yaml") ]
 }
 
-output "cluster_security_group_id" {
-  description = "Security group ID of the control plane in the cluster"
-  value       = module.eks.cluster_security_group_id
-}
+resource "helm_release" "edge" {
+  
+  name       = "cribl-edge"
+  repository = "https://criblio.github.io/helm-charts/"
+  chart      = "edge"
+  version    = "${var.cribl_edge_version}"
+  namespace  = "cribl"
+  create_namespace = true
 
-output "region" {
-  description = "AWS region"
-  value       = var.region
-}
+  depends_on = [null_resource.update_kubeconfig]
 
-output "oidc_provider_arn" {
-  value = module.eks.oidc_provider_arn
+  set {
+    name  = "cribl.leader"
+    value = "tls://${var.cribl_edge_token}@${var.cribl_edge_leader_url}?group=${var.cribl_edge_fleet}"
+  }
+
+  set {
+    name  = "env.CRIBL_K8S_TLS_REJECT_UNAUTHORIZED"
+    value = "0"
+  }
+
+  values = [ file("${path.module}/../../cribl/edge/values.yaml") ]
 }
