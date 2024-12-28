59e092daad5872291c6862514f83bc16bf2fc0bc
MH <mh @gmail com>
2024-12-28 18:23:10 +1100

    Displaying the URLs as outputs

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -273,3 +273,35 @@ resource "kubectl_manifest" "customm_dashboard" {
   force_conflicts = true
   depends_on = [kubectl_manifest.elastic_stack]
 }
+
+# Use kubernetes provider to retrieve the external IPs
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    command     = "aws"
+  }
+}
+
+data "kubernetes_service" "cribl_worker_service" {
+  metadata {
+    name      = "cribl-worker-logstream-workergroup"
+    namespace = "cribl"
+  }
+}
+
+data "kubernetes_service" "kibana_service" {
+  metadata {
+    name      = "kibana-kb-http"
+    namespace = "elastic"
+  }
+}
+
+data "kubernetes_service" "app_service" {
+  metadata {
+    name      = "opentelemetry-demo-frontendproxy"
+    namespace = "otel-demo"
+  }
+}
---
+++ terraform/eks/outputs.tf
@@ -0,0 +1,14 @@
+output "cribl_worker_external_url" {
+  description = "URL to access the Cribl Worker for Search replays"
+  value = format("Cribl Worker URL: http://%s:10200", data.kubernetes_service.cribl_worker_service.status.0.load_balancer.0.ingress.0.hostname)
+}
+
+output "kibana_external_url" {
+  description = "URL to access Kibana"
+  value = format("Kibana URL: http://%s:5601", data.kubernetes_service.kibana_service.status.0.load_balancer.0.ingress.0.hostname)
+}
+
+output "app_external_url" {
+  description = "URL to access the otel-demo app and loadgen (/loadgen/) UI"
+  value = format("otel-demo App URL: http://%s:8080", data.kubernetes_service.app_service.status.0.load_balancer.0.ingress.0.hostname)
+}
