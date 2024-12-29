9264bb5e74ed9d452d975783d0cc650338622290
MH <mh @gmail com>
2024-12-29 21:34:00 +1100

    simplified provisioning with exec

--- terraform/eks/otel-demo.tf
+++ terraform/eks/otel-demo.tf
@@ -1,11 +1,10 @@
 # Install otel-demo app
-data "kubectl_file_documents" "otel_demo_manifests" {
-  content = file("${path.module}/../../otel-demo/opentelemetry-demo.yaml")
-}
-
-resource "kubectl_manifest" "opentelemetry_demo" {
-  for_each  = toset(data.kubectl_file_documents.otel_demo_manifests.documents)
-  yaml_body = each.value
-  override_namespace = "otel-demo"
-  depends_on = [ null_resource.wait_for_cluster ]
+resource "null_resource" "provision_otel_demo_app" {
+    depends_on = [ null_resource.wait_for_cluster ]
+    
+    provisioner "local-exec" {
+        command = <<EOF
+            kubectl apply -n otel-demo -f ${path.module}/../../otel-demo/opentelemetry-demo.yaml
+        EOF
+    }
 }
