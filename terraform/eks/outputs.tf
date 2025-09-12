7e0df4633015b1efeff0ed9ce4589f3317355fd3
MH <mh @gmail com>
2025-09-12 18:24:46 +1000

    Added more output vars

--- terraform/eks/outputs.tf
+++ terraform/eks/outputs.tf
@@ -12,3 +12,13 @@ output "app_external_url" {
   description = "URL to access the otel-demo app and loadgen (/loadgen/) UI"
   value = format("otel-demo App URL: http://%s:8080", data.kubernetes_service.app_service.status.0.load_balancer.0.ingress.0.hostname)
 }
+
+output "cluster_name" {
+  description = "EKS Cluster Name"
+  value       = module.eks.cluster_name
+}
+
+output "region" {
+  description = "AWS Region"
+  value       = var.region
+}
