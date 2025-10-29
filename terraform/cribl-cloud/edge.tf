c48ab9f06af469e5e82e82c931bb9fe39f1cab1d
MH <mh @gmail com>
2025-10-29 15:57:19 +1100

    Remove unused OpenTelemetry and Cribl TCP configuration files; update edge.tf to define resources directly

--- terraform/cribl-cloud/edge.tf
+++ terraform/cribl-cloud/edge.tf
@@ -13,49 +13,110 @@ resource "criblio_group" "k8s_edge_fleet" {
   worker_remote_access = true
 }
 
-locals {
-    source_otel_grpc_json                   = jsondecode(file("./config/source_otel_grpc.json"))
-    source_otel_http_json                   = jsondecode(file("./config/source_otel_http.json"))
-    destination_disk_spool_otel_spool_json  = jsondecode(file("./config/destination_disk_spool_otel_spool.json"))
-    destination_cribl_tcp_json              = jsondecode(file("./config/destination_cribl_tcp.json"))
-}
-
 # Create disk spool destinations
 resource "criblio_destination" "destination_disk_spool_otel_spool" {
-  id       = local.destination_disk_spool_otel_spool_json.id
+  id       = "otel-spool"
   group_id = criblio_group.k8s_edge_fleet.id
 
-  # Disk Spool configuration from JSON
-  output_disk_spool = local.destination_disk_spool_otel_spool_json
+  output_disk_spool = {
+        id = "otel-spool"
+        type = "disk_spool"
+  }
 }
 
 # Create Cribl TCP destination
 resource "criblio_destination" "destination_cribl_tcp" {
-    id       = local.destination_cribl_tcp_json.id
+    id       = "cribl-tcp"
     group_id = criblio_group.k8s_edge_fleet.id
 
-    # Cribl TCP configuration from JSON
-    output_cribl_tcp = local.destination_cribl_tcp_json
+    output_cribl_tcp = {
+        id = "cribl-tcp"
+        type = "cribl_tcp"
+        host = "cribl-worker-logstream-workergroup"
+        port = 10300
+        load_balanced = false
+        hosts = [
+            {
+                host = "cribl-worker-logstream-workergroup"
+                port = 10300
+            }
+        ]
+    }
 }
 
 # Create GRPC OTel source
 resource "criblio_source" "source_otel_grpc" {
-  id       = local.source_otel_grpc_json.id
+  id       = "otel-grpc"
   group_id = criblio_group.k8s_edge_fleet.id
 
-  # OpenTelemetry configuration from JSON
-  input_open_telemetry = local.source_otel_grpc_json
+  input_open_telemetry = {
+    id              = "otel-grpc"
+    type            = "open_telemetry"
+    protocol        = "grpc"
+    version         = "1.3.1"
+    extract_logs    = true
+    extract_metrics = true
+    extract_traces  = true
+    send_to_routes  = false
+    connections = [ 
+        {
+            output = "cribl-tcp"
+        },
+        {
+            output = "otel-spool"
+        }
+    ]
+  }
 
   depends_on = [ criblio_destination.destination_cribl_tcp, criblio_destination.destination_disk_spool_otel_spool ]
 }
 
 # Create HTTP OTel source
 resource "criblio_source" "source_otel_http" {
-  id       = local.source_otel_http_json.id
+  id       = "otel-http"
   group_id = criblio_group.k8s_edge_fleet.id
 
-  # OpenTelemetry configuration from JSON
-  input_open_telemetry = local.source_otel_http_json
+    input_open_telemetry = {
+    id              = "otel-http"
+    type            = "open_telemetry"        
+    protocol        = "http"
+    version         = "1.3.1"
+    port            = 4318
+    extract_logs    = true
+    extract_metrics = true
+    extract_traces  = true
+    send_to_routes  = false
+    connections = [ 
+        {
+            output = "cribl-tcp"
+        },
+        {
+            output = "otel-spool"
+        }
+    ]
+  }
 
   depends_on = [ criblio_destination.destination_cribl_tcp, criblio_destination.destination_disk_spool_otel_spool ]
+}
+
+
+# Commit and deploy the configuration
+data "criblio_config_version" "edge_configversion" {
+  id         = criblio_group.k8s_edge_fleet.id
+  depends_on = [criblio_commit.edge_commit]
+}
+
+resource "criblio_commit" "edge_commit" {
+  effective = true
+  group     = criblio_group.k8s_edge_fleet.id
+  message   = "Automated Edge configuration commit"
+
+  depends_on = [ criblio_source.source_otel_grpc, criblio_source.source_otel_http ]
+}
+
+resource "criblio_deploy" "edge_deploy" {
+  id      = criblio_group.k8s_edge_fleet.id
+  version = data.criblio_config_version.edge_configversion.items[0]
+
+  depends_on = [ criblio_commit.edge_commit ]
 }
