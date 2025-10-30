a713eddf17d0f8a2a1d1554ae31574c7bda2685d
MH <mh @gmail com>
2025-10-30 23:09:00 +1100

    Add initial search.tf configuration for OTel demo Search pack

---
+++ terraform/cribl-cloud/search.tf
@@ -0,0 +1,10 @@
+# Doesn't work yet
+
+# resource "criblio_pack" "search_pack" {
+#     id              = "cribl-k8s-otel-demo"
+#     group_id        = "default_search"
+#     display_name    = "OTel demo Search pack"
+#     filename        = "cribl-k8s-otel-demo_0.0.3.crbl"
+#     version         = "0.0.2"
+#     description     = "OTel demo Search pack with dashboards"
+# }
